ac01ace97df79e1ac084b0358cab4139991bcc88
Mohammed Al M <mohammedwed@icloud.com>
2023-02-25 10:05:25 +0200

    removed authentication

--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.default, name='default'),
-    path("signup/", views.signup, name="signup"),
-    path("login/", views.login_auth, name="login_auth"),  
-    path("logout/", views.logout_auth, name="logout_auth"),
     path("playlist/", views.playlist, name='your_playlists'),
     path("search/", views.search, name='search_page') 
 ]
--- main/views.py
+++ main/views.py
@@ -15,8 +15,7 @@ CONTAINER = json.load(f)
 
 def default(request):
     global CONTAINER
-    if request.user.is_anonymous:
-        return redirect('/login')
+
 
     if request.method == 'POST':
 
@@ -26,67 +25,9 @@ def default(request):
     song = 'kSFJGEHDCrQ'
     return render(request, 'player.html',{'CONTAINER':CONTAINER, 'song':song})
 
-def signup(request):
-    context= {'username':True,'email':True}
-    if not request.user.is_anonymous:
-        return redirect('/')
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-
-
-        if (username,) in User.objects.values_list("username",) :
-            context['username'] = False
-            return render(request,'signup.html',context)
-
-        elif (email,) in User.objects.values_list("email",):
-            context['email'] = False
-            return render(request,'signup.html',context)
-
-        playlist_user.objects.create(username=username)
-        new_user = User.objects.create_user(username,email,password)
-        new_user.save()
-        login(request,new_user)
-        return redirect('/')
-    return render(request,'signup.html',context)
-
-
-def login_auth(request):
-    if not request.user.is_anonymous:
-        return redirect('/')
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        # print(User.objects.values_list("password",))
-
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            # A backend authenticated the credentials
-            login(request,user)
-            return redirect('/')
-
-        else:
-            # No backend authenticated the credentials
-            context= {'case':False}
-            return render(request,'login.html',context)
-
-
-    context= {'case':True}
-    return render(request,'login.html',context)
-
-
-
-def logout_auth(request):
-    logout(request)
-    return redirect('/login')
 
 
 def playlist(request):
-    if request.user.is_anonymous:
-        return redirect('/login')
     cur_user = playlist_user.objects.get(username = request.user)
     try:
       song = request.GET.get('song')
